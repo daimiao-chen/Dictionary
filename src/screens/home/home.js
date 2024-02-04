6c5a9add4dd86c5e64a9f081c61791d2602bb3c7
NonyeAsogwa <n_asogwa@fanshaweonline.ca>
2024-02-03 22:59:39 -0500

    includes TextInput and search button

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -2,24 +2,36 @@ import React from 'react';
 import { View } from 'react-native';
 import { PaperProvider, Button, Portal, Modal } from 'react-native-paper';
 import { StyleSheet } from 'react-native';
-
+import { TextInput } from 'react-native';
 import { WordCard } from '../../components/wordCard/wordCard';
 
 export const Home = ({isDark}) => {
   const [displayWord, setDisplayWord] = React.useState(false);
+  const [searchText, setSearchText] = React.useState('');
+  const handleSearch = () => {
+    setDisplayWord(true);
+  };
 
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <Button mode="contained" onPress={() => setDisplayWord(true) }> Search </Button>
+        <TextInput
+          style={styles.input}
+          placeholder = "Type here to translate!"
+          value={searchText}
+          onChangeText={text => setSearchText(text)}
+          />
+
+        <Button mode="contained" onPress={handleSearch}> Search </Button>
         <Portal>
             <Modal visible={displayWord} onDismiss={() => setDisplayWord(false)}>
-              <WordCard word="abandon" isDark={isDark} />
+              <WordCard word={searchText} isDark={isDark} />
             </Modal>
         </Portal>
       </View>
     </PaperProvider>
   );
+  
 }
 
 const styles = StyleSheet.create({
@@ -29,4 +41,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 20,
+    borderWidth: 1,
+    padding: 10,
+  }
 });
