d9287113375b27c68bc996071917650d23d5fa13
NonyeAsogwa <n_asogwa@fanshaweonline.ca>
2024-03-04 05:55:06 -0500

    Update favourite.js
    added some function

--- src/screens/favourite/favourite.js
+++ src/screens/favourite/favourite.js
@@ -1,40 +1,76 @@
 import React from 'react';
-import { View, FlatList } from 'react-native';
-import { Button, Card, Text, PaperProvider } from 'react-native-paper';
+import { View, FlatList, TextInput, StyleSheet } from 'react-native';
+import { Checkbox, PaperProvider } from 'react-native-paper';
 import * as wordDB from '../../utils/word';
 import { WordItem } from '../../components/wordCard/wordCard';
 
 export const Favourite = () => {
   const [favouriteList, setFavouriteList] = React.useState([]);
-
-  const favouriteListener = (results) => {
-    console.log('Favourite listener', results);
-    setFavouriteList(results);
-  };
+  const [filterText, setFilterText] = React.useState('');
+  const [checkedItems, setCheckedItems] = React.useState({});
 
   React.useEffect(() => {
-    wordDB.registerFavouriteListener(favouriteListener);
-
-    return () => {
-      wordDB.unregisterFavouriteListener(favouriteListener);
+    const favouriteListener = (listenerCallback) => {
+      const fetchData = () => {
+        // Simulated data for testing
+        const testData = [
+          { id: 1, word: 'Favorite Word 1' },
+          { id: 2, word: 'Favorite Word 2' },
+          { id: 3, word: 'Favorite Word 3' },
+        ];
+        
+        listenerCallback(testData);
+      };
+  
+      setInterval(fetchData, 5000); 
     };
+  
+    // Usage:
+    favouriteListener(setFavouriteList);
   }, []);
 
-  const buttonListener = () => {
-    wordDB.pickRandomWord()
-      .then(result => {
-        wordDB.addFavourite(result.word);
-      });
+  const handleCheck = (itemId) => {
+    setCheckedItems(prevState => ({
+      ...prevState,
+      [itemId]: !prevState[itemId] 
+    }));
   };
+  
+  const filteredList = favouriteList.filter(item =>
+    item.word.toLowerCase().includes(filterText.toLowerCase())
+  );
 
   return (
     <PaperProvider>
       <FlatList
-        data={favouriteList}
-        renderItem={({ item }) => <WordItem item={item} />}
-        keyExtractor={(item) => item.id.toString()} 
+        data={filteredList}
+        renderItem={({ item }) => (
+         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+           <Checkbox
+              status={checkedItems[item.id] ? 'checked' : 'unchecked'}
+              onPress={() => handleCheck(item.id)}
+           />
+            <WordItem item={item} />
+          </View>
+        )}
+        keyExtractor={(item) => item.id.toString()}
       />
+      <TextInput
+       placeholder="Filter"
+        style={styles.Filter}
+        value={filterText}
+        onChangeText={setFilterText}
+       />
     </PaperProvider>
   );
 };
+
+const styles = StyleSheet.create({
+    Filter: {
+      height: 40,
+      borderWidth: 1,
+      margin: 10,
+      padding: 10,
+    },
+  });
 
