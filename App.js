1d68a4b3b525b5d62b8d98804afcb84e70f81582
Oleg Baryshev <o_baryshev@fanshaweonline.ca>
2024-04-09 21:43:34 -0400

    icons have been added to the bottom navigation for each screen

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { Favourite } from './src/screens/favourite/favourite';
 import { Test } from './src/screens/test/test';
 import { Settings } from './src/screens/settings/settings';
 import { LogBox } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'; // импорт иконок
+
 LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
 LogBox.ignoreAllLogs();//Ignore all log notifications
 
@@ -14,13 +16,48 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Favourite" component={Favourite} />
-        <Tab.Screen name="Test" component={Test} />
-        <Tab.Screen name="Settings" component={Settings} />
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: '#525CEB', 
+          inactiveTintColor: '#B0B0B0', 
+        }}>
+        <Tab.Screen 
+          name="Home" 
+          component={Home} 
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Favourite" 
+          component={Favourite} 
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="favorite" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Test" 
+          component={Test} 
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="test" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Settings" 
+          component={Settings} 
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="settings" color={color} size={size} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
